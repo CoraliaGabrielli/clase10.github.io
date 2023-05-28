59476c0e4f6638557cd376e01b9425d47d9083df
CoraliaGabrielli <130628214+CoraliaGabrielli@users.noreply.github.com>
2023-05-28 03:11:11 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+let nombre = "Coralia";
+let apellido = "Gabrielli"
+
+console.log(nombre + " " + apellido);
+
+let numero1 = 21;
+let numero2= 10;
+let suma = numero1 + numero2;
+
+console.log(suma);
+
+let nuevavariable = suma + 2021;
+console.log(nuevavariable);
